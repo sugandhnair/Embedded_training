2e495181a6b95173ea7567f50611e21e0f7edac1
SUGANDH D NAIR <61353024+sugandhnair@users.noreply.github.com>
2021-06-16 19:29:53 +0530

    Update dec_to_bin.c

--- 16-06-2021/dec_to_bin.c
+++ 16-06-2021/dec_to_bin.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int find(int decimal_number)
+void find(int decimal_number)
 {
     if (decimal_number == 0){
-        return 0;
+        return;
     }
     else{
-        return (decimal_number % 2 + 10 * find(decimal_number / 2));
+        find(decimal_number/2);
+        printf("%d\n",decimal_number%2);
     }
 }
  
@@ -15,6 +16,7 @@ int main()
     int decimal_number;
     printf("Enter the decimal number : ");
     scanf("%d",&decimal_number);
-    printf("%d", find(decimal_number));
+    find(decimal_number);
     return 0;
 }
+
